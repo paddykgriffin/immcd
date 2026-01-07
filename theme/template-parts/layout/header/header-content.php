6ad98a25cb5a5600604c5e42895fa430b690c90d
Paddy Griffin <paddygriffin@gmail.com>
2026-01-07 12:38:23 +0000

    Fix - header

--- theme/template-parts/layout/header/header-content.php
+++ theme/template-parts/layout/header/header-content.php
@@ -13,20 +13,20 @@
 
 	<div class="container max-w-[96rem]"> 
 
-		<div class="grid grid-cols-2 lg:grid-cols-12 gap-6 md:gap-6 justify-between">
+		<div class="grid grid-cols-2 md:grid-cols-12 gap-6 justify-between">
 
-			<div class="lg:col-span-11 flex gap-6 items-center">
+			<div class="md:col-span-6 lg:col-span-11 flex gap-6 items-center ">
 				<?php
 				if ( is_front_page() ) :
 					?>
 						<a href="<?php echo esc_url(home_url('/')); ?>" rel="home" class="hover:opacity-50 transition-all duration-300">
-								<img src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="<?php bloginfo('name'); ?>" class="w-16 md:w-24" />
+								<img src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="<?php bloginfo('name'); ?>" class="w-16 lg:w-24" />
 							</a>	
 					<?php
 				else :
 					?>
 						<a href="<?php echo esc_url(home_url('/')); ?>" rel="home" class="hover:opacity-50 transition-all duration-300">
-								<img src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="<?php bloginfo('name'); ?>" class="w-16 md:w-24" />
+								<img src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="<?php bloginfo('name'); ?>" class="w-16 lg:w-24" />
 							</a>	
 					<?php
 				endif;
@@ -37,30 +37,27 @@
 					<p><?php echo $immanuel_church_dublin_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 				<?php endif; ?>
 			
-
-			<nav id="site-navigation" aria-label="<?php esc_attr_e( 'Main Navigation', 'immanuel-church-dublin' ); ?>" class="pl-6 flex">
-				<!-- <button aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'immanuel-church-dublin' ); ?></button> -->
-
-				<?php
-				wp_nav_menu(
-					array(
-						'theme_location' => 'menu-1',
-									'container' => false,
-									'menu_id' => 'primary-menu', 
-									'menu_class' => ' gap-12 lg:gap-8 2xl:gap-12 hidden xl:flex', 
-									'items_wrap' => '<ul id="%1$s" class="%2$s" aria-label="submenu">%3$s</ul>',
-					)
-				);
-				?>
-			</nav><!-- #site-navigation -->
+				<nav id="site-navigation" aria-label="<?php esc_attr_e( 'Main Navigation', 'immanuel-church-dublin' ); ?>" class="pl-6 flex ">
+					<?php
+					wp_nav_menu( 
+						array(
+							'theme_location' => 'menu-1',
+							'container' => false,
+							'menu_id' => 'primary-menu', 
+							'menu_class' => ' gap-12 lg:gap-8 2xl:gap-12 hidden lg:flex ', 
+							'items_wrap' => '<ul id="%1$s" class="%2$s" aria-label="submenu">%3$s</ul>',
+						)
+					);
+					?>
+				</nav><!-- #site-navigation -->
 			</div>
 
-			<div class="lg:col-span-1 grid items-center">
+			<div class="md:col-span-6 lg:col-span-1 grid items-center">
 				<div class="flex justify-end">
-					<div class="hidden xl:block">
+					<div class="hidden lg:block">
 						<?php get_template_part('template-parts/layout/header/header', 'mode'); ?>
 					</div>
-					<button id="menuBtn" class="xl:hidden text-black dark:text-white" aria-controls="primary-menu"
+					<button id="menuBtn" class="lg:hidden text-black dark:text-white" aria-controls="primary-menu"
 						aria-expanded="false">
 						<span class="material-symbols-outlined !block !text-[40px]">menu</span>
 						<p class="sr-only"> <?php esc_html_e('Primary Menu', '_bless'); ?>
@@ -73,3 +70,4 @@
 	</div><!-- .container -->
 
 </header><!-- end #masthead -->
+ 
